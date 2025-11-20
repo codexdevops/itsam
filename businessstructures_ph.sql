2a37e4e4fbd8c7d2e50b8fd850aa5c457a01ffbe
Eugene Magno <80873441+eugenemagno@users.noreply.github.com>
2025-11-20 06:47:06 -0600

    Update businessstructures_ph.sql

--- businessstructures_ph.sql
+++ businessstructures_ph.sql
@@ -1,5 +1,99 @@
-INSERT [gen].[BusinessStructure] ([BusinessStructureId],[Code],[Description],[CountryId],[IssuedByAgency])VALUES(1,'SOLE','Sole Proprietorship',175,'Department of Trade and Industry (DTI)')
-INSERT [gen].[BusinessStructure] ([BusinessStructureId],[Code],[Description],[CountryId],[IssuedByAgency])VALUES(2,'PARTNERSHIP','Partnership',175,'Department of Trade and Industry (DTI)')
-INSERT [gen].[BusinessStructure] ([BusinessStructureId],[Code],[Description],[CountryId],[IssuedByAgency])VALUES(3,'OPC','One Person Corporation (OPC)',175,'Securities and Exchange Commission (SEC)')
-INSERT [gen].[BusinessStructure] ([BusinessStructureId],[Code],[Description],[CountryId],[IssuedByAgency])VALUES(4,'CORP','Corporation',175,'Securities and Exchange Commission (SEC)')
-INSERT [gen].[BusinessStructure] ([BusinessStructureId],[Code],[Description],[CountryId],[IssuedByAgency])VALUES(5,'COOP','Cooperative',175,'Cooperative Development Authority (CDA)')
+INSERT INTO gen.BusinessStructure
+(BusinessStructureId, Code, Description, CountryId, IssuedByAgency)
+VALUES
+(1, 'B_CORP', 'Benefit Corporation (B-Corp)', 236,
+ 'State Secretary of State; Optional B Lab Certification'),
+
+(2, 'BARANGAY', 'Barangay Micro Business Enterprise (BMBE)', 175,
+ 'Department of Trade and Industry (DTI)'),
+
+(3, 'C_CORP', 'C Corporation', 236,
+ 'State Secretary of State; Internal Revenue Service (IRS); FinCEN'),
+
+(4, 'CLOSE_CORP', 'Close Corporation', 236,
+ 'State Secretary of State'),
+
+(5, 'COOP', 'Cooperative (Co-op)', 236,
+ 'State Secretary of State; USDA'),
+
+(6, 'COOP_PH', 'Cooperative', 175,
+ 'Cooperative Development Authority (CDA)'),
+
+(7, 'FOREIGN_ENTITY_US', 'Foreign Corporation or Foreign LLC', 236,
+ 'State Secretary of State; IRS; FinCEN'),
+
+(8, 'FOREIGN_ENTITY_PH', 'Foreign Corporation (Philippines)', 175,
+ 'Securities and Exchange Commission (SEC)'),
+
+(9, 'FRANCHISE', 'Franchise Business Model', 236,
+ 'Federal Trade Commission (FTC)'),
+
+(10, 'GP', 'General Partnership', 236,
+ 'State Secretary of State; IRS'),
+
+(11, 'GEN_PARTNERSHIP_PH', 'General Partnership', 175,
+ 'Department of Trade and Industry (DTI)'),
+
+(12, 'HOLDING_COMP', 'Holding Company', 236,
+ 'State Secretary of State'),
+
+(13, 'JOINT_VENTURE', 'Joint Venture (Contract-Based)', 236,
+ 'Contractual Agreement (No issuing agency)'),
+
+(14, 'LLC', 'Limited Liability Company', 236,
+ 'State Secretary of State; IRS'),
+
+(15, 'LLP', 'Limited Liability Partnership', 236,
+ 'State Secretary of State; Professional Boards'),
+
+(16, 'LP', 'Limited Partnership', 236,
+ 'State Secretary of State'),
+
+(17, 'L3C', 'Low-Profit Limited Liability Company (L3C)', 236,
+ 'State Secretary of State'),
+
+(18, 'NON_PROFIT_PH', 'Non-Stock / Non-Profit Corporation', 175,
+ 'Securities and Exchange Commission (SEC)'),
+
+(19, 'NONPROFIT_PRIVATE_FOUND', 'Private Foundation (501(c)(3))', 236,
+ 'Internal Revenue Service (IRS)'),
+
+(20, 'NONPROFIT_PUBLIC_CHARITY', 'Public Charity (501(c)(3))', 236,
+ 'IRS; State Charities Bureau'),
+
+(21, 'PUBLIC_CORP', 'Publicly Traded Corporation', 236,
+ 'SEC (Securities and Exchange Commission)'),
+
+(22, 'PC', 'Professional Corporation', 236,
+ 'State Secretary of State; Licensing Boards'),
+
+(23, 'PLLC', 'Professional Limited Liability Company', 236,
+ 'State Secretary of State; Licensing Boards'),
+
+(24, 'SPC', 'Social Purpose Corporation / Public Benefit Corporation', 236,
+ 'State Secretary of State'),
+
+(25, 'S_CORP', 'S Corporation', 236,
+ 'IRS; State Secretary of State'),
+
+(26, 'SERIES_LLC', 'Series LLC', 236,
+ 'State Secretary of State'),
+
+(27, 'SINGLE_PROP_PH', 'Sole Proprietorship', 175,
+ 'Department of Trade and Industry (DTI)'),
+
+(28, 'SOLE_PROP_US', 'Sole Proprietorship', 236,
+ 'State/Local Government; IRS'),
+
+(29, 'SMLLC', 'Single-Member LLC', 236,
+ 'State Secretary of State; IRS'),
+
+(30, 'TRIBAL_ENTITY', 'Tribal Business Entity', 236,
+ 'Tribal Government; BIA'),
+
+(31, 'TRUST_BUS', 'Business Trust / Massachusetts Trust', 236,
+ 'State Secretary of State'),
+
+(32, 'RELIGIOUS_NONPROFIT', 'Religious Corporation', 236,
+ 'State Secretary of State; IRS')
+;
